55e71eba591fa2e092cceebdb785f94992167e75
OkashaTanoli <okashatanoli12345@gmail.com>
2022-08-02 21:58:22 +0500

    second final touch

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 
 function App() {
   return (
+    // <div className='BeforeTestDiv'>
+    //   <h1 className='BeforeTestDivHead'>FG BOYS INTER COLLEGE KARACHI CANTT</h1>
+    //   <div className='BeforeTestDivCenter'>
+    //     <h1>Admission Will Open On (4th August)</h1>
+    //   </div>
+    // </div>
     <PagesRouter />
   );
 }
