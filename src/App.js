65bab920adee2b05e8a8f904c3b709fa91d56dba
OkashaTanoli <okashatanoli12345@gmail.com>
2022-08-02 22:36:08 +0500

    form closed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import PagesRouter from './components/Router/router';
+// import PagesRouter from './components/Router/router';
 import './App.css';
 
 function App() {
   return (
-    // <div className='BeforeTestDiv'>
-    //   <h1 className='BeforeTestDivHead'>FG BOYS INTER COLLEGE KARACHI CANTT</h1>
-    //   <div className='BeforeTestDivCenter'>
-    //     <h1>Admission Will Open On (4th August)</h1>
-    //   </div>
-    // </div>
-    <PagesRouter />
+    <div className='BeforeTestDiv'>
+      <h1 className='BeforeTestDivHead'>FG BOYS INTER COLLEGE KARACHI CANTT</h1>
+      <div className='BeforeTestDivCenter'>
+        <h1>Admission Will Open On (4th August)</h1>
+      </div>
+    </div>
+    // <PagesRouter />
   );
 }
 
